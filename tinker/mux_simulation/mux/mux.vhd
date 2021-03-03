6677d5bf0d7af7904e0691201aa4de3fc4a5b5e6
Gabriel <gtlchaves@gmail.com>
2021-03-03 09:48:58 -0300

    Reduz mux para 32 - 2

--- tinker/mux_simulation/mux/mux.vhd
+++ tinker/mux_simulation/mux/mux.vhd
@@ -3,17 +3,16 @@ use IEEE.std_logic_1164.all;
 
 entity mux is
     port(
-            a, b, c, d : in std_logic_vector(31 downto 0);
-            sel : in std_logic_vector(1 downto 0);
+            a, b : in std_logic_vector(31 downto 0);
+            sel : in std_logic;
             e : out std_logic_vector(31 downto 0));
 end mux;
 
 architecture mux_arc of mux is
+    constant u_vec : std_logic_vector(31 downto 0) := (others => 'U');
     begin
     with sel select
-        e <= a when "00",
-             b when "01",
-             c when "10",
-             d when "11",
-             "--------------------------------" when others;
+        e <= a when '0',
+             b when '1',
+             u_vec when others;
 end mux_arc;
