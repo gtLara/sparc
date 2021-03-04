417e0123c8fd197ad9dab6304e7226e7bc01e3b2
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 17:27:22 -0300

    Update paridade.asm

--- tinkerASM/paridade.asm
+++ tinkerASM/paridade.asm
@@ -3,7 +3,7 @@
 ! jmpl, com reg destino %g0 - pro for (salto incondicional)
 ! add - pro for
 ! cmp a,b = subcc a, b, %g0 - para mudar o icc e usar o bge
-! bge,a - "Branch on Greater or Equal" com annul ",a" pro for
+! bl,a - "Branch on Less" com annul ",a" pro for
 ! srl
 ! xor
 ! and
@@ -19,5 +19,19 @@
 
 !seções devem ser iniciadas assim:
 .section ".data"
-
+    dados:  .word 4    !pode ser qualquer coisa
+    crc:    .word 1    !se dados = 0, crc = 1 e paridade par
+    
 .section ".text"
+
+main:   ld crc , %l0        ! traz os dados da memória de dados
+        ld dados , %l1      ! para os registradores locais
+        
+        add %l7, %g0, %l7   ! inicia %l7 = i = 0        
+
+for:    and %l1, 1, %l2     ! pega o LSB do dados atual e poe em %l2
+        srl %l1, 1, %l1     ! dados >>= 1 
+
+        add %l7, 1, %l7     ! incrementa i
+        cmp %l7 , 8         ! compara i com 8 e modifica o icc
+        bl,a for            ! se i < 8 volta pro for, sem a instrução de delay
