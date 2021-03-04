dc5e69446ff463cf859d55cf59c4498fbfa670fb
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 14:25:06 -0300

    Update paridade.c

--- tinkerASM/paridade.c
+++ tinkerASM/paridade.c
@@ -1,1 +1,22 @@
+/*
+ * Programa de teste de paridade: crc = 1 paridade par e crc = 0 paridade ímpar
+ */
+#include <stdio.h>
 
+int crc(){
+  int crc = 1;//se tudo for zero, o crc não se altera e a paridade é par
+  int dados = 0xfb;//somente 8 bits são usados
+  
+  for(int i = 0; i < 8; i++){
+    //faz XOR do CRC com o bit i dos dados (começando do bit zero)
+    /*
+     * exemplo
+     * dados = 0xfb;
+     * dados >> 2 = 0x3e;
+     * ( (dados >> 2) & 0x1 ) = 0;
+     *
+     */
+    crc ^= ((dados >> i)& 0x1);
+  }
+  return crc;
+}
