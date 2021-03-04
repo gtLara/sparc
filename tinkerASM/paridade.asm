dfb44940ecc1a1a579d87b3d6bd20341dac54560
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 16:39:21 -0300

    Update paridade.asm

--- tinkerASM/paridade.asm
+++ tinkerASM/paridade.asm
@@ -8,12 +8,13 @@
 ! xor
 ! and
 
-! dilaceramos o banco de registradores, só usamos 32:
+! dilaceramos o banco de registradores, só usamos 32 + 1:
 ! %g0 ~ %g7 = %r0  ~ %r7   - registradores globais
 ! %o0 ~ %07 = %r8  ~ %r15  - registradores Out
 ! %l0 ~ %l7 = %r16 ~ %r23 - registradores locais
 ! %i0 ~ %i7 = %r24 ~ %r31 - registradores in
 ! %g0 = %r0 = constante 0
+! %PSR - registrador de estado, usa ele no bge
 ! tem o PC tbm, mas ele sempre existe
 
 !seções devem ser iniciadas assim:
