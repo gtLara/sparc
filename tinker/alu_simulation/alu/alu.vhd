4b7f1cad59ce542856d1e96cdf9063f471b7cda9
Gabriel <gtlchaves@gmail.com>
2021-03-04 11:58:30 -0300

    Corrige bandeira zero

--- tinker/alu_simulation/alu/alu.vhd
+++ tinker/alu_simulation/alu/alu.vhd
@@ -6,44 +6,50 @@ use ieee.NUMERIC_STD.all;
 
 entity alu is
     port(
-        src_a : in std_logic_vector(3 downto 0);
-        src_b: in std_logic_vector(3 downto 0);
-        alu_control : in std_logic_vector(1 downto 0);
-        alu_result : out std_logic_vector(3 downto 0);
+        src_a : in std_logic_vector(31 downto 0);
+        src_b: in std_logic_vector(31 downto 0);
+        alu_control : in std_logic_vector(3 downto 0);
+        alu_result : out std_logic_vector(31 downto 0);
         zero : out std_logic);
 
 end alu;
 
 architecture alu_arc of alu is
 
-signal result : std_logic_vector(3 downto 0);
-constant all_zeros : std_logic_vector(3 downto 0) := (others => '1');
+signal result : std_logic_vector(31 downto 0);
+constant all_zeros : std_logic_vector(31 downto 0) := (others => '0');
 
 begin
     alu_process: process(alu_control, src_a, src_b)
         begin
             case (alu_control) is
 
-                when "00" => result <= src_a + src_b;
-                when "01" => result <= src_a - src_b;
-                when "10" => result <= src_a OR src_b;
-                when "11" => result <= src_a XOR src_b;
-                -- preencher outras operacoes de acordo com necessidade
-                when others => result <= "UUUU";
+                when "0000" => result <= src_a + src_b; -- soma
+                when "0001" => result <= src_a - src_b; -- subtracao
+                when "0010" => result <= src_a OR src_b; -- or
+                when "0011" => result <= src_a XOR src_b; -- xor
+                when "0100" =>                            -- set less than
+                    if (src_a < src_b) then
+                        result <= all_zeros;
+                        result(0) <= '1';
+                    else
+                        result <= all_zeros; -- pega caso de igualdade!
+                    end if;
+
+                -- tratar mais casos se necessario
+
+                when others => null; -- no op 
+                    result <= "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU";
 
             end case;
 
             -- saida zero se o resultado for zero
 
-            if(result = all_zeros) then
-                zero <= '1';
-            else
-                zero <= '0';
-            end if;
-
         end process;
 
-
         alu_result <= result;
+
+        zero <= '1' when result = all_zeros else
+                '0';
 
 end alu_arc;
--- tinker/alu_simulation/alu/tb_alu.vhd
+++ tinker/alu_simulation/alu/tb_alu.vhd
@@ -13,64 +13,80 @@ architecture tb of tb_alu is
 
     component alu is
         port(
-            src_a : in std_logic_vector(3 downto 0);
-            src_b: in std_logic_vector(3 downto 0);
-            alu_control : in std_logic_vector(1 downto 0);
-            alu_result : out std_logic_vector(3 downto 0);
+            src_a : in std_logic_vector(31 downto 0);
+            src_b: in std_logic_vector(31 downto 0);
+            alu_control : in std_logic_vector(3 downto 0);
+            alu_result : out std_logic_vector(31 downto 0);
             zero : out std_logic);
     end component;
 
-    -- inputs
+        -- inputs
 
-    signal src_a : std_logic_vector(3 downto 0) := (others => '0');
-    signal src_b : std_logic_vector(3 downto 0) := (others => '0');
-    signal alu_control : std_logic_vector(1 downto 0) := (others => '0');
-    
-    -- outputs
+        signal src_a : std_logic_vector(31 downto 0) := (others => '0');
+        signal src_b : std_logic_vector(31 downto 0) := (others => '0');
+        signal alu_control : std_logic_vector(3 downto 0) := (others => '0');
 
-    signal alu_result : std_logic_vector(3 downto 0);
-    signal zero : std_logic;
+        -- outputs
 
-    begin
+        signal alu_result : std_logic_vector(31 downto 0);
+        signal zero : std_logic;
 
-    -- instancia unidade sob teste
-
-    uut: alu port map(
-                        src_a => src_a,
-                        src_b => src_b,
-                        alu_control => alu_control,
-                        alu_result => alu_result,
-                        zero => zero);
-    
-    -- executa testbench, variando entradas para verificar saidas
-
-    test_bench: process
-        constant period: time := 100 ns;
         begin
-            wait for period;
 
-            src_a <= "1111";
+        -- instancia unidade sob teste
 
-            wait for period;
-            
-            alu_control <= "01";
-            src_b <= "1111";
+        uut: alu port map(
+                            src_a => src_a,
+                            src_b => src_b,
+                            alu_control => alu_control,
+                            alu_result => alu_result,
+                            zero => zero);
 
-            wait for period;
+        -- executa testbench, variando entradas para verificar saidas
 
-            alu_control <= "11";
-            src_a  <= "1010";
+        test_bench: process
 
-            wait for period;
+            constant period: time := 100 ns;
+            begin
+                wait for period;
 
-            alu_control <= "10";
-            src_b  <= "1010";
-            src_a  <= "1110";
+                src_a <= "11111111100000000000000000000000";
+                src_b <= "11111111111111111111111111111111";
 
-            wait for period;
+                wait for period;
 
-        end process;
+                alu_control <= "0000";
+
+                wait for period;
+
+                alu_control <= "0001";
+
+                wait for period;
+
+                alu_control <= "0010";
+
+                wait for period;
+
+                alu_control <= "0011";
+
+                wait for period;
+
+                alu_control <= "0100";
+
+                wait for period;
+
+                src_b <= "11111111100000000000000000000000";
+                src_a <= "11111111111111111111111111111111";
+
+                alu_control <= "0100";
+
+                wait for period;
+
+                src_a <= "11111111111111111111111111111111";
+                src_b <= "11111111111111111111111111111111";
+
+                alu_control <= "0001";
+
+            end process;
 
 end tb;
-
--- TODO: investigar erro em output "zero"
