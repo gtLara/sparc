e3a20a016cdeebe5c3397258009322da6f7d338d
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 15:01:28 -0300

    Update paridade.asm

--- tinkerASM/paridade.asm
+++ tinkerASM/paridade.asm
@@ -2,9 +2,9 @@
 ! <nome> significa que não foi selecionada a instrução SPARC
 
 ! ld - pra iniciar variáveis
-! <jumpl> ou <call> - pro for
+! <jumpl> ou <call> - pro for (acho que nem vai precisar)
 ! add - pro for
-! Bicc - pro for
+! BGE - "Branch on Greater or Equal" pro for
 ! srl
 ! xor
 ! and
