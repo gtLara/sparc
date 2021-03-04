620be401b32453b6f70722afc957415a9c2da91f
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 17:33:11 -0300

    Versão 1 pronta
    A princípio pronto, gostaria que vcs conferissem se fiz algo de errado.

--- tinkerASM/paridade.asm
+++ tinkerASM/paridade.asm
@@ -14,7 +14,7 @@
 ! %l0 ~ %l7 = %r16 ~ %r23 - registradores locais
 ! %i0 ~ %i7 = %r24 ~ %r31 - registradores in
 ! %g0 = %r0 = constante 0
-! %PSR - registrador de estado, usa ele no bge
+! %psr - registrador de estado, usa ele no bl
 ! tem o PC tbm, mas ele sempre existe
 
 !seções devem ser iniciadas assim:
@@ -30,8 +30,12 @@ main:   ld crc , %l0        ! traz os dados da memória de dados
         add %l7, %g0, %l7   ! inicia %l7 = i = 0        
 
 for:    and %l1, 1, %l2     ! pega o LSB do dados atual e poe em %l2
+        xor %l0, %l2, %l0   ! atualiza o crc com o bit de dados
         srl %l1, 1, %l1     ! dados >>= 1 
 
         add %l7, 1, %l7     ! incrementa i
         cmp %l7 , 8         ! compara i com 8 e modifica o icc
         bl,a for            ! se i < 8 volta pro for, sem a instrução de delay
+!FIM, o resultado com crc fica em %l0
+        
+        printf("%d",crc);
