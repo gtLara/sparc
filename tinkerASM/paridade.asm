6a6ac9f1c4381a6c65aff65f6f4746431584c6d5
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 15:07:32 -0300

    Update paridade.asm

--- tinkerASM/paridade.asm
+++ tinkerASM/paridade.asm
@@ -1,6 +1,5 @@
 ! Instruções a serem usadas:
 ! <nome> significa que não foi selecionada a instrução SPARC
-
 ! ld - pra iniciar variáveis
 ! <jumpl> ou <call> - pro for (acho que nem vai precisar)
 ! add - pro for
@@ -8,3 +7,11 @@
 ! srl
 ! xor
 ! and
+
+! dilaceramos o banco de registradores, só usamos 32:
+! %g0 ~ %g7 = %r0  ~ %r7   - registradores globais
+! %o0 ~ %07 = %r8  ~ %r15  - registradores Out
+! %l0 ~ %l7 = %r16 ~ %r23 - registradores locais
+! %i0 ~ %i7 = %r24 ~ %r31 - registradores in
+
+
