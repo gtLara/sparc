94ddcbce2c294875f2abc8ccc366c4287da44b01
Gabriel P. G <gabpgomes@gmail.com>
2021-03-04 14:30:14 -0300

    Update paridade.asm

--- tinkerASM/paridade.asm
+++ tinkerASM/paridade.asm
@@ -1,1 +1,10 @@
+! Instruções a serem usadas:
+! <nome> significa que não foi selecionada a instrução SPARC
 
+! <soma imediato> ou <carrega memória> - pra iniciar variáveis
+! <branch> ou <jump> - pro for
+! add - pro for
+! <compara> - pro for
+! <shift right>
+! <XOR>
+! <AND>
