0cb513f8ce951b4535b7ff4745bb931a464a4efb
Gabriel <gtlchaves@gmail.com>
2021-03-06 01:10:45 -0300

    Adiciona testbench

--- tinker/sparc/tb_sparc.vhd
+++ tinker/sparc/tb_sparc.vhd
@@ -15,9 +15,29 @@ architecture tb of tb_sparc is
             );
     end component;
 
+    constant clock_frequency : integer := 10e6; -- 10Mhz
+    constant clock_period : time := 1000 ms /clock_frequency;
+
+    signal clk : std_logic := '1';
+    signal reset : std_logic := '0';
+    constant period: time := 200 ns;
 
     begin
 
-    -- para inciar desenvolvimento de tb precisa de algum tipo de controle!
+    uut: sparc port map(
+                        clk  => clk ,
+                        reset => reset
+                        );
+
+    clk <= not clk after clock_period / 2;
+
+    testbench: process
+        begin
+
+        wait for period;
+
+        wait for period;
+
+    end process testbench;
 
 end tb;
