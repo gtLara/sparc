a0fd3a949890d9d4616c832dba9202d29167e829
Gabriel <gtlchaves@gmail.com>
2021-03-09 19:21:58 -0300

    generaliza entrada mux

--- tinker/components/mux_simulation/mux/mux.vhd
+++ tinker/components/mux_simulation/mux/mux.vhd
@@ -2,14 +2,15 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity mux is
+    generic (size : integer := 32);
     port(
-            a, b : in std_logic_vector(31 downto 0);
+            a, b : in std_logic_vector(size - 1 downto 0);
             sel : in std_logic;
-            e : out std_logic_vector(31 downto 0));
+            e : out std_logic_vector(size - 1 downto 0));
 end mux;
 
 architecture mux_arc of mux is
-    constant u_vec : std_logic_vector(31 downto 0) := (others => 'U');
+    constant u_vec : std_logic_vector(size - 1 downto 0) := (others => 'U');
     begin
     with sel select
         e <= a when '0',
