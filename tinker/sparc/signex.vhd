6d5e7ce4066e5089248d4f8df648888da67be5fb
Gabriel <gtlchaves@gmail.com>
2021-03-09 16:31:06 -0300

    Generaliza entrada de signex

--- tinker/sparc/signex.vhd
+++ tinker/sparc/signex.vhd
@@ -4,20 +4,21 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.NUMERIC_STD.all;
 
 entity signex is
+    generic(size: integer := 12); -- na verdade é tamanho - 1
     port(
-         signex_in: in std_logic_vector(12 downto 0);
+         signex_in: in std_logic_vector(size  downto 0);
          signex_out: out std_logic_vector(31 downto 0));
 end signex;
 
 architecture signex_arc of signex is
     constant one : std_logic := '1';
     constant zero : std_logic := '0';
-    constant ones_extension : std_logic_vector(18 downto 0) := (others => '1');
-    constant zeros_extension : std_logic_vector(18 downto 0) := (others => '0');
+    constant ones_extension : std_logic_vector(31 - size - 1 downto 0) := (others => '1');
+    constant zeros_extension : std_logic_vector(31 - size - 1 downto 0) := (others => '0');
     constant u_vec : std_logic_vector(31 downto 0) := (others => 'U');
 
     begin
-        with (signex_in(12)) select
+        with (signex_in(size)) select
             signex_out <= ones_extension & signex_in when one,
                           zeros_extension & signex_in when zero,
                           u_vec when others;
